be30bf4885623f73844b0b4296925300a6d4a13e
chloesnyder <csny@seas.upenn.edu>
2014-04-27 13:57:19 -0400

    Hw 6
    What’s working: game ends and slashes.
    To Do:
    the null pointer exception on level and score labels
    implement physics
    implement cool animations
    fix the divide by 0!

---
+++ HW10 6/src/Shape.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics2D;
+
+
+/** An interface for shapes that can be drawn on the screen */
+public interface Shape {
+	
+	/** Display the shape on the screen */
+	public void draw(Graphics2D gc);
+	
+	
+}
